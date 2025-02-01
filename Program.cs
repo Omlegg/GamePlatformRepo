aca2f8ababc08162d14ee9d6c542971cde156592
nurlanamj <nurlanaem@code.edu.az>
2025-02-01 17:07:19 +0400

    initialize project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/games/", () => {
+    return new {
+        Name = "Fortnite",
+        Impression = 15
+    };
+});
+
+app.Run();
